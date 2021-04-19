00ef70420a199babad9aac963ca1044bd14b7bf0
JonasWalsted <45150246+JonasWalsted@users.noreply.github.com>
2021-04-19 10:20:50 +0200

    Update app

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Introduktion from './introduktion/Introduktion';
+import Introduktion from './1introduktion/Introduktion';
 import GoogleAuth from './GoogleAuth';
+import Demografi from './Demografi';
 
 const PageOne = () => {
   return (
     <div>
-
       <Introduktion />
       <GoogleAuth />
-      <Link to="/pagetwo"> Navigate </Link>
     </div>
   );
 };
 
 const PageTwo = () => {
   return <div>
-    PageTwo
-    <Link to="/"> Navigate </Link>
+    <Demografi />
   </div>
 };
 
