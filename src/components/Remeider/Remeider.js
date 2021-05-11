805135500facfdf866854b3da86aff961b62f14b
JonasWalsted <45150246+JonasWalsted@users.noreply.github.com>
2021-05-11 11:44:00 +0200

    sad

---
+++ src/components/Remeider/Remeider.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Remeider.css';
+import history from '../../history';
+import { Link } from 'react-router-dom';
+import '../Template.css';
+
+const Remeider() {
+    const imageClick() {
+        return {
+            history.push('/PageFour')
+
+        }
+
+    }
+    render() {
+        return (
+            <div className="body" >
+                <div className="Design">
+                    <div className="tekstHeader">
+                        <h1>Velkommen</h1>
+                    </div>
+                    <div className="Merlin_box3">
+                        <div className="bubble">
+                            <p>Hej! Jeg hedder Merlin <br /> Velkommen til dette forsøg</p>
+                            <div className="trekantVedTaleboble"></div>
+                        </div>
+                        <img className="Merlin" src="Billede1.png" alt="Merlin"></img>
+                    </div>
+
+                    <div className="tilbage">
+                        <button className="large ui back button">
+                            <Link to="/PageTwo"> Tilbage </Link>
+                        </button>
+
+                        <div className="Naste">
+                            <button className="large ui next button">
+                                <Link to="/PageFour"> Næste </Link>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="Billede1">
+                        <img onClick={this.imageClick} className="Skruetrakker" src="Skruetrakker.png" alt="Skruetrækker"></img>
+                    </div>
+
+
+                </div>
+            </div >
+        )
+    }
+}
+
+export default Remeider;
