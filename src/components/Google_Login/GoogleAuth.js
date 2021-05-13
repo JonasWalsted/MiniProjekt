cedc6c122b6873907a0975cc28ab7b487ee33a1b
JonasWalsted <45150246+JonasWalsted@users.noreply.github.com>
2021-05-13 16:19:30 +0200

    Update GoogleAuth.js

--- src/components/Google_Login/GoogleAuth.js
+++ src/components/Google_Login/GoogleAuth.js
@@ -13,13 +13,12 @@ class GoogleAuth extends React.Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.onAuthChange(this.auth.isSignedIn.get())
-                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
     onAuthChange = (isSignedIn) => {
-        if (isSignedIn) {
+        if (isSignedIn===true) {
             this.props.signIn((this.auth.currentUser.get().getId()));
         } else {
             this.props.signOut();
@@ -37,7 +36,7 @@ class GoogleAuth extends React.Component {
     renderAuthButton() {
         if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.props.isSignedIn === true) {
+        }  else if (this.props.isSignedIn === true) {
             return (
                 <button onClick={this.onSignOut} className="ui red google button">
                     <i className="google icon">
